73aebb756168afa3d374cac0157410129f243651
Zlh-4431 <124013738+Zlh-4431@users.noreply.github.com>
2024-02-07 22:03:27 +0800

    1

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const p1 = document.getElementById("p1");
 const h1 = document.getElementById("h1");
 const butt = document.getElementById("butt");
 
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 不包含最大值，包含最小值
+}
+
 function random() {
-    p1.innerHTML = Math.random(1,100);
+    p1.innerHTML = getRandomInt(1,101);
 }
 
 butt.addEventListener("click", () => {
