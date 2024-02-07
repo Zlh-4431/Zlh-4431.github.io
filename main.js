ae2114028dd9eed318c73badf5d6b918bf66d79d
Zlh-4431 <124013738+Zlh-4431@users.noreply.github.com>
2024-02-07 21:59:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const h1 = document.getElementById("h1");
 const butt = document.getElementById("butt");
 
 function random() {
-    p1.innerHTML = Math.random();
+    p1.innerHTML = Math.random(1,100);
 }
 
 butt.addEventListener("click", () => {
